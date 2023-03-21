e495585610b4dd2b733b66635cd57ef288fb2c3c
byreece-254 <byroneaustine@gmail.com>
2023-03-21 15:52:32 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -26,7 +26,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(",");
 		}
@@ -36,7 +36,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(",");
 		}
