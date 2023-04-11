611f8df690d1f42eae2df3ac6e63c2d77de625f1
byreece-254 <byroneaustine@gmail.com>
2023-04-11 10:50:58 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,11 +9,11 @@
  */
 char *_strdup(char *str)
 {
-	char*aaa;
-	int i, r=0;
+	char *aaa;
+	int i, r = 0;
 
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
